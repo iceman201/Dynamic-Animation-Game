c904e1a089afbf2f7c4913c3e735cfb0d9ff261e
Liguo Jiao <liguojiao@liguos-imac.mckay.co.nz>
2015-11-26 12:08:22 +1300

    DA-200-Gravity and collision
    - add gravity and collision behaviour

--- Dynamic Animation Game/ViewController.swift
+++ Dynamic Animation Game/ViewController.swift
@@ -13,23 +13,38 @@ class ViewController: UIViewController {
     @IBAction func dropDown(sender: AnyObject) {
         drop()
     }
-    @IBOutlet weak var smallBox: UIView!
+    var dropRow = 10
+    let gravity = UIGravityBehavior()
+    
     var dropSize: CGSize {
         let size = backgroundView.bounds.size.width / CGFloat(dropRow)
         return CGSize(width: size, height: size)
     }
-    var dropRow = 10
-    
+    lazy var animator: UIDynamicAnimator = {
+        let createDynamicAnimator = UIDynamicAnimator(referenceView: self.backgroundView)
+        return createDynamicAnimator
+    }()
+    lazy var collider: UICollisionBehavior = {
+        let createCollider = UICollisionBehavior()
+        createCollider.translatesReferenceBoundsIntoBoundary = true
+        return createCollider
+    }()
     func drop() {
         var frame = CGRect(origin: CGPointZero, size: dropSize)
         frame.origin.x = CGFloat.random(dropRow) * dropSize.width
-        print(frame.origin.x, terminator: "")
+        
+        print(frame.origin.x)
+        
         let dropView = UIView(frame: frame)
         dropView.backgroundColor = UIColor.random
         backgroundView.addSubview(dropView)
+        gravity.addItem(dropView)
+        collider.addItem(dropView)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        animator.addBehavior(gravity)
+        animator.addBehavior(collider)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
